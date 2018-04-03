5545af35645afef5479aed9a41b20deb087e89e0
Danielle Howard <edanielleh@gmail.com>
2018-04-02 21:37:48 -0400

    Added Background Image, Text, Buttons, Added CSS File

---
+++ client/components/home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from "prop-types";
+ // <img src= {'css/background.jpg'}/>
+
+class Home extends React.Component {
+    render() {
+        return ( 
+            <div className= "profile-container">
+                <div className= "profile-text">
+                    <h1>I'm Danielle Howard</h1>
+                    <h5>An Aspiring Full Stack Developer</h5>
+                    <h5>Graduate of Grace Hopper</h5>
+                </div>
+                <div className= "btn-row">
+                    <button className = "btn btn-secondary">GitHub</button>
+                    <button className = "btn btn-secondary">LinkedIn</button>
+                </div>
+            </div>
+        );
+    };
+};
+
+export default Home;
