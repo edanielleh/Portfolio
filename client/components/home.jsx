75a3eb16d23b06356f0c32b29a16cf196a39d507
Danielle Howard <edanielleh@gmail.com>
2018-04-04 15:47:06 -0400

    CSS Fix for Navbar

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from "prop-types";
-import '../../public/css/background.css';
+// import '../../public/css/background.css';
 
 class Home extends React.Component {
     constructor(props) {
